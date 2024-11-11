b01145e4650261d04b53e32fb78daf1d35d665f7
Rodrigo Castellanos Rodríguez <141855228+Rodrigocr04@users.noreply.github.com>
2024-11-11 11:50:10 -0600

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+// src/App.tsx
+import React from 'react';
+import LoginPage from './src/screen-Login/LoginPage';
+import HomeStudentsPage from './src/screen-HomeStudents/HomeStudentsPage';
+import SupportPage from './src/screen-Soporte/SoporteScreen';
+import SeguridadScreen from './src/screen-Seguridad/SeguridadScreen';
+
+const App: React.FC = () => {
+  return <LoginPage />;
+};
+
+export default App;
